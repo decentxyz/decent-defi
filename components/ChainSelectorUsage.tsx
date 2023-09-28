4abe481ba4951c7fa753a814eaba14a042e0b174
arshan <arshankhanifar@gmail.com>
2023-09-27 21:34:09 -0400

    henlo

--- components/ChainSelectorUsage.tsx
+++ components/ChainSelectorUsage.tsx
@@ -26,6 +26,7 @@ export const ChainSelectorUsage = ({
             srcChainId={chainId}
             setSrcChainId={setChainId}
             chains={chains}
+            title={"⛓️"}
           />
         </div>
       </div>
